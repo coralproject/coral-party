3e3511c7a22e048a01ea31f09a40daf97672f19b
nick-funk <code@nickfunk.com>
2024-07-19 10:30:26 -0600

    set `export` in the `next.config.js`
    https://nextjs.org/docs/app/building-your-application/deploying/static-exports

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
+        output: "export",
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
         "react-dom": "preact/compat",
